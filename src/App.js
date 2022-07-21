d97b5aac5432f72a94fcf3e7fa7faa01d353ea12
Zabi-Ullah <71921004+Zabi-Ullah@users.noreply.github.com>
2022-07-21 11:21:44 +0500

    hello netlify funtion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useEffect,useState } from "react";
 
 
 function App() {
+  const [val,setVal]=useState()
+  useEffect(()=>{
+    fetch(`/.netlify/functions/hello?name=zabi-ullah`)
+    .then(res=>res.json())
+    .then(obj=>setVal(obj))
+  },[])
+
   return (
     <div >
-      <h1>React Project Deploying netlify its Live from github </h1>
+      <h1> {val && val.message} in React Project Deploying netlify its Live from github </h1>
     </div>
   );
 }
